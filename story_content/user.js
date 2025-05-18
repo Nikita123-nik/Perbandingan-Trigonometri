f573fed56084756af41b1d5e8b239f8c6c3c4bab
Nikita123-nik <hinadonumetrolina@gmail.com>
2025-05-18 19:32:56 +0800

    Perbandingan Trigonometri

---
+++ story_content/user.js
@@ -0,0 +1,165 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Oxdn0eSZdz":
+        Script1();
+        break;
+      case "6Y7SPNpibQk":
+        Script2();
+        break;
+      case "5w1G3SGhoe4":
+        Script3();
+        break;
+      case "6C5WOG2c0Fm":
+        Script4();
+        break;
+      case "6NAR1iFWtep":
+        Script5();
+        break;
+      case "6j4i8ZM6szq":
+        Script6();
+        break;
+      case "5lscJP6ju9W":
+        Script7();
+        break;
+      case "6ni17NchzfA":
+        Script8();
+        break;
+      case "61u3sX9X6UW":
+        Script9();
+        break;
+      case "6MqiOxFNxXm":
+        Script10();
+        break;
+      case "5XiDlphIVeI":
+        Script11();
+        break;
+      case "6mW4YFP2Yr5":
+        Script12();
+        break;
+      case "6QxJnzIZk1i":
+        Script13();
+        break;
+      case "5mo3yJJTMmi":
+        Script14();
+        break;
+      case "6LWJPz2ubGY":
+        Script15();
+        break;
+      case "5Z7WGOWCH2J":
+        Script16();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="1.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="1.mp3";
+audio.load();
+audio.play();
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="2.mp3";
+audio.load();
+audio.play();
+audio.volume
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="3.mp3";
+audio.load();
+audio.play();
+audio.volume
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="4.mp3";
+audio.load();
+audio.play();
+audio.volume
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.0;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.1;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.2;
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.3;
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.4;
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.5;
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.6;
+}
+
+function Script13()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.7;
+}
+
+function Script14()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.8;
+}
+
+function Script15()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.9;
+}
+
+function Script16()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 1;
+}
+
